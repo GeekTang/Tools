8460b2cf05ca0a1b7169cdfc75d050f3d3ec1c4b
Ares Tang <ares.tang@gmail.com>
2013-02-21 15:55:49 +0800

    Add Java Swing project for log view

---
+++ log.view/src/main/java/igeektang/com/log/view/App.java
@@ -0,0 +1,13 @@
+package igeektang.com.log.view;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
